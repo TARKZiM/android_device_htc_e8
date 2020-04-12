fb8272c640c65cfc93ceb6d2e5559542a1ceb26b
Bruno Martins <bgcngm@gmail.com>
2020-04-12 12:13:10 +0100

    e8: Preserve previously set TARGET_RECOVERY_DEVICE_MODULES
    TARGET_RECOVERY_DEVICE_MODULES is also defined in the common
    tree and should not be overridden.
    
    Change-Id: I00846f86ef6514e547cd166649fb4686167a74fc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2015 The CyanogenMod Project
-#               2017 The LineageOS Project
+#               2017,2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -56,7 +56,7 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 13153337344
 
 # Vendor Init
 TARGET_INIT_VENDOR_LIB := libinit_e8
-TARGET_RECOVERY_DEVICE_MODULES := libinit_e8
+TARGET_RECOVERY_DEVICE_MODULES += libinit_e8
 
 # Inherit from the proprietary version
 -include vendor/htc/e8/BoardConfigVendor.mk
